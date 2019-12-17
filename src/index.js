1064734d49fd42f69acfdd53f6287a5304f7fc48
Brad Cordeiro <brad.cordeiro@gmail.com>
2019-12-17 15:52:12 -0800

    make requests more idomatic

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ const { request } = require('https');
 const querystring = require('querystring');
 const xml2js = require('xml2js');
 
-const endpointBase = 'https://api.prowlapp.com/publicapi/';
-
 function prowlRequest(endpoint, params, method) {
   const formattedParams = querystring.encode(params);
 
-  const APIrequest = request(`${endpointBase}/${endpoint}?${formattedParams}`, method);
+  const options = {
+    host: 'api.prowlapp.com',
+    path: `/publicapi/${endpoint}?${formattedParams}`,
+    method,
+  };
 
   return new Promise((resolve, reject) => {
-    APIrequest.on('response', (response) => {
+    const APIrequest = request(options, (response) => {
       response.setEncoding('utf-8');
-
       let xml = '';
+
       response.on('data', (chunk) => { xml += chunk; });
       response.on('error', (error) => reject(error));
       response.on('end', () => {
@@ -40,22 +42,13 @@ function send(apikey, application, options) {
   return prowlRequest('add', parameters, 'POST');
 }
 
-async function isKeyValid(apikey, providerkey) {
-  let result;
-
-  if (Array.isArray(apikey)) {
-    return Promise.all(apikey.map((key) => isKeyValid(key, providerkey)));
-  }
-
+function verify(apikey, providerkey) {
   if (providerkey) {
-    result = await prowlRequest('verify', { apikey, providerkey }, 'GET');
+    return prowlRequest('verify', { apikey, providerkey }, 'GET');
   }
 
-  result = await prowlRequest('verify', { apikey }, 'GET');
-
-  return result.success !== undefined;
+  return prowlRequest('verify', { apikey }, 'GET');
 }
-const areKeysValid = isKeyValid;
 
 function retrieveToken(providerkey) {
   return prowlRequest('retrieve/token', { providerkey }, 'GET');
@@ -65,10 +58,28 @@ function retrieveAPIKey(providerkey, token) {
   return prowlRequest('retrieve/apikey', { providerkey, token }, 'GET');
 }
 
+async function isKeyValid(apikey, providerkey) {
+  if (Array.isArray(apikey)) {
+    return Promise.all(apikey.map((key) => isKeyValid(key, providerkey)));
+  }
+
+  let result;
+
+  if (providerkey) {
+    result = await verify(apikey, providerkey);
+  }
+
+  result = await verify(apikey);
+
+  return result.success !== undefined;
+}
+const areKeysValid = isKeyValid;
+
 module.exports = {
   send,
-  isKeyValid,
-  areKeysValid,
+  verify,
   retrieveToken,
   retrieveAPIKey,
+  isKeyValid,
+  areKeysValid,
 };
